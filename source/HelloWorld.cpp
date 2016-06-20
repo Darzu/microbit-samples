4832f3a64fd09529ff10c84afc06e41317fc4158
darzu <daryl.zuniga@gmail.com>
2016-06-20 15:34:20 -0700

    trying to target nRF51-DK board

--- source/HelloWorld.cpp
+++ source/HelloWorld.cpp
@@ -35,8 +35,10 @@ int main()
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    // Insert your code here!
-    uBit.display.scrollbackwards("HELLO WORLD! :)");
+    while(true) {
+      // Insert your code here!
+      uBit.display.scrollbackwards("HELLO WORLD! :)");
+    }
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
